ef9e82e66f945ac77cbdf44707d20e1f1167984c
gocersensei <gocersensei@gmail.com>
2020-12-16 01:57:21 +0000

    This program computes the sum of the first n positive integers

--- freeFall.py
+++ freeFall.py
@@ -1,9 +1,3 @@
-#Create a program that determines how quickly an object is traveling when it hits the
-#ground. The user will enter the height from which the object is dropped in meters (m).
-#Because the object is dropped its initial speed is 0m/s. Assume that the acceleration
-#due to gravity is 9.8m/s2. You can use the formula vf = vi2 + 2ad to compute the
-#final speed, vf , when the initial speed, vi, acceleration, a, and distance, d, are known.
-
 #Compute the speed of an object when it hits the ground after being dropped.
 
 from math import sqrt
@@ -22,3 +16,6 @@ vf = sqrt(2*GRAVITY*d)
 
 #Display the result
 print("It will hit the ground at %2f m/s." % vf)
+
+
+
