970bcb48ccf1cd7605210d52805b04c066697300
gocersensei <gocersensei@gmail.com>
2021-01-15 14:45:20 +0000

    Computes the number of unique characters in a string using a dictionary

---
+++ uniqueChracters.py
@@ -0,0 +1,17 @@
+#
+# Compute the number of unique characters in a string using a dictionary
+#
+
+# Read the string from the user
+s = input("Enter a string: ")
+
+# Add each character to a dictionary with a value of True.
+# Once we are done the number of keys in the dictionary
+# will be the number of unique characters in the string.
+characters = {}
+for ch in s:
+    characters[ch] = True
+
+# Display the result
+print("That string contained", len(characters), "unique character(s).")
+
