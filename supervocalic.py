56f272a5e5f3cd9903b1a89545d76ecae1fbe883
gocersensei <gocersensei@gmail.com>
2021-03-09 14:56:03 +0000

    initial commit

---
+++ supervocalic.py
@@ -0,0 +1,7 @@
+def get_sv(filename):
+    vowels = {'a', 'e', 'i', 'o', 'u'}
+
+    return {word.strip()
+
+                for word in open(filename)
+                if vowels < set(word.lower())}
